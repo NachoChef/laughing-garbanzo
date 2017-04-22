5ee3e68d9644ebc9a48ddd75c72a409933d163f7
Justin Jones <hock3ydud3@gmail.com>
2017-04-22 15:42:18 -0500

    Update projecteuler.py

--- projecteuler.py
+++ projecteuler.py
@@ -14,18 +14,6 @@ def  isPrime(n):
             return False
     return True
 
-def  isPrime2(n):
-    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
-    """ Input n>=6, Returns a array of primes, 2 <= p < n """
-    sieve = np.ones(n/3 + (n%6==2), dtype=np.bool)
-    sieve[0] = False
-    for i in range(int(n**0.5)//3+1):
-        if sieve[i]:
-            k=3*i+1|1
-            sieve[      ((k*k)/3)      ::2*k] = False
-            sieve[(k*k+4*k-2*k*(i&1))/3::2*k] = False
-    return np.r_[2,3,((3*np.nonzero(sieve)[0]+1)|1)]
-
 #return sum of digits
 def  d(k):
     sum = 0
@@ -97,26 +85,3 @@ def  collatz(n):
 start = time.time()
 print(arithDer(primeFactors(20)))
 print(time.time() - start)
-'''
-The arithmetic derivative is defined by
-
-p' = 1 for any prime p
-(ab)' = a'b + ab' for all integers a, b (Leibniz rule)
-For example, 20' = 24
-
-Find ∑ gcd(k,k') for 1 < k ≤ 5·1015
-
-Note: gcd(x,y) denotes the greatest common divisor of x and y.
-
-+++++++++++++++++++++
-
-There are some prime values, p, for which there exists a positive integer, n, such that
-the expression n3 + n2p is a perfect cube.
-
-For example, when p = 19, 83 + 82×19 = 123.
-
-What is perhaps most surprising is that for each prime with this property the value of n is
-unique, and there are only four such primes below one-hundred.
-
-How many primes below one million have this remarkable property?
-'''
